ba46d235f4a9e18d3f1a7ce4b47cb2d1dd500025
Wadd Franklin <waddinsohn@gmail.com>
2017-10-09 15:18:48 -0300

    Adicionando arquivos huffman_tree.c e huffman_tree.h e correção da main.c (#3)
    * Criando main.c e os arquivos .c e .h da fila de prioridades
    
    * Atualizando o .gitignore
    
    * - Adicionando a função compress_file() na main.c
    - Criando a biblioteca para funções de controle de tela screen.c e screen.h
    - Adicionando a função clear_screen() na screen.h para limpar a tela
    
    * Corrigindo especificações
    
    * - Adicionando a função open_file_test() para testar se o arquivo foi aberto com êxito
    - Adicionando a função frequency_table() que lê um arquivo em modo binário e faz a contagem dos bytes no array frequency_array[]
    - Começando a implementar a função compress_file()
    - Adicionando novas definições para unsigned char (u_char) e unsigned int (u_int)
    
    * Adicionando *.layout ao .gitignore
    
    * - Adicionando a documentações em screen.h e compress.h
    
    * Corrigindo erro de referência da função compress_file em compress.h
    
    * - Adicionada a biblioteca "compress.h" ao main.c
    - Criados os arquivos huffman_tree.c e huffman_tree.h
    - Criação das funções create_empty_huffman_tree e create_huffman_tree
    - Adicionado a definiao de tipo de huffman_tree para h_tree

--- compress.c
+++ compress.c
@@ -28,7 +28,9 @@ void frequency_table (FILE *file, u_int *frequency_array) {
 void compress_file () {
 
     FILE *imput_file = NULL;                  /* ponteiro para o arquivo de entrada */
-    char imput_file_name[MAX_STR_LEN];      /* string para guardar o nome do arquivo de entrada */
+
+    char imput_file_name[MAX_STR_LEN];        /* string para guardar o nome do arquivo de entrada */
+  
     u_int frequency_array[MAX_ARR_LEN] = {0}; /* array de frequência dos bytes do arquivo de entrada */
     u_int i;                                  /* controlador de loop */
 
@@ -52,9 +54,9 @@ void compress_file () {
 
     frequency_table(imput_file, frequency_array);
 
-    //for (i = 32 ; i <= 126 ; i++) {
-    //    if (frequency_array[i] != 0) {
-    //        printf("%c - %d\n", i, frequency_array[i]);
-    //    }
-    //}
+    for (i = 32 ; i <= 126 ; i++) {
+        if (frequency_array[i] != 0) {
+            printf("%c - %d\n", i, frequency_array[i]);
+        }
+    }
 }
