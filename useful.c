5a2b5cb6e727166638ebc429303a15b91d758a27
Wadd_Franklin <waddinsohn@gmail.com>
2017-10-21 11:37:37 -0300

    Adicionando função para printar um arquivo binário na tela (para DEBUG)

--- useful.c
+++ useful.c
@@ -11,3 +11,14 @@ u_char set_bit (u_char byte, int bit_index) {
 
     return (byte | (1 << (7 - bit_index)));
 }
+
+void print_binary_file () {
+
+    u_char byte;
+    FILE *pfile = fopen("teste.huff", "rb");
+
+    while (fread(&byte, sizeof(u_char), 1, pfile) == 1) {
+
+        printf("[%c]", byte);
+    }
+}
